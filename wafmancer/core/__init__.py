1261624aca5ea018ba108c84824ebab420b4583d
Abhijith S <abh78i00@gmail.com>
2026-04-30 10:23:45 +0530

    Update __init__.py

--- wafmancer/core/__init__.py
+++ wafmancer/core/__init__.py
@@ -1,10 +1,27 @@
 """
 WAFMANCER Core Engine.
-Contains the Response Oracle, HTTP client, and differential analysis engine.
+Contains the Response Oracle, HTTP client, differential analysis engine,
+WAF fingerprinting, and smart mutation generation.
 """
 
-from wafmancer.core.oracle import ResponseOracle
-from wafmancer.core.diff_engine import AdvancedDiffEngine
-from wafmancer.core.http_client import AsyncResearchClient
+from wafmancer.core.oracle import ResponseOracle, OracleSession, ProbeResult
+from wafmancer.core.diff_engine import AdvancedDiffEngine, DiffResult, AnomalySeverity
+from wafmancer.core.http_client import AsyncResearchClient, ResearchRequest, ResearchResponse
+from wafmancer.core.fingerprinter import WAFFingerprinter, WAFSignature, WAFVendor
+from wafmancer.core.mutation_engine import SmartMutationEngine
 
-__all__ = ["ResponseOracle", "AdvancedDiffEngine", "AsyncResearchClient"]
+__all__ = [
+    "ResponseOracle",
+    "OracleSession",
+    "ProbeResult",
+    "AdvancedDiffEngine",
+    "DiffResult",
+    "AnomalySeverity",
+    "AsyncResearchClient",
+    "ResearchRequest",
+    "ResearchResponse",
+    "WAFFingerprinter",
+    "WAFSignature",
+    "WAFVendor",
+    "SmartMutationEngine",
+]
